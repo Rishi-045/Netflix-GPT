555ae7c98de8a8136ad89eb886a5ae8799db8c35
Rishi-045 <palrishikesh2004@gmail.com>
2025-07-02 12:57:43 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='text-center text-bold text-5xl'>
+        heloo bachooooo....
+       </div>
+    </>
+  )
+}
+
+export default App
