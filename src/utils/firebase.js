a230a1dae827b82c6e46eaba5c7e45e366d1b290
Rishi-045 <palrishikesh2004@gmail.com>
2025-07-05 18:51:18 +0530

    moved Firebase config apiKey to .env file

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth} from "firebase/auth";
+const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,7 +11,7 @@ import { getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDF_OERyt6LZOCJAnzvW5sdpmQcNgzIvGc",
+  apiKey: apiKey,
   authDomain: "netflix-gpt-f45f7.firebaseapp.com",
   projectId: "netflix-gpt-f45f7",
   storageBucket: "netflix-gpt-f45f7.firebasestorage.app",
