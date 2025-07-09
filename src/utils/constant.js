4ae7b00ebbec41ca5db580b950bc603943d24378
Rishi-045 <palrishikesh2004@gmail.com>
2025-07-09 15:17:08 +0530

    place access token inside .env

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,9 @@
+const accessToken = import.meta.env.VITE_ACCESS_TOKEN_TMDB
+
 export const API_OPTION = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTljOGNmM2ZlNjQ4NGE1NGExZjhiMmYzNjdhZWVlZCIsIm5iZiI6MTc1MTg4NjQ4Ny44NTUsInN1YiI6IjY4NmJhYTk3ZTA5N2Y3NTViODEwM2FiYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WAScarngu-PU91GfoL2oECRp6IcyJcWQIjGnr6MtQZo'
+    Authorization: `Bearer ${accessToken} `
   }
 };
